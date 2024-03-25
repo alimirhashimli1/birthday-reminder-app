7d11c6daf43812e6069d5816a03aaad21e4b9d7d
alimirhashimli <alimirhashimli@gmail.com>
2024-03-25 15:31:23 +0100

    returned to the previous code in birthday details and deleted the url blob fetch

--- frontend/src/components/BirthdayDetails.js
+++ frontend/src/components/BirthdayDetails.js
@@ -1,54 +1,22 @@
 import React, { useState, useEffect } from "react";
 
 const BirthdayDetails = ({ birthday }) => {
-  const [loading, setLoading] = useState(true);
-  const [imageSrc, setImageSrc] = useState(null);
   const date = new Date(birthday.birthdate);
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
 
-  useEffect(() => {
-    const loadImage = async () => {
-      try {
-        const response = await fetch(birthday.picture);
-        const blob = await response.blob();
-        const url = URL.createObjectURL(blob);
-        setImageSrc(url);
-      } catch (error) {
-        console.error("Error loading image:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    loadImage();
-
-    return () => {
-      // Clean up if necessary (e.g., revoke object URL)
-      if (imageSrc) {
-        URL.revokeObjectURL(imageSrc);
-      }
-    };
-  }, [imageSrc]);
-
   return (
     <div className="birthday-details">
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          <h4>
-            {birthday.name} {birthday.surname}
-          </h4>
-          <p>
-            <strong>Birthday on </strong>
-            {day}-{month}-{year}
-          </p>
-          <p className="birthday-note">{birthday.note}</p>
-          <img src={birthday.picture} alt="" width="100" height="100" />
-        </>
-      )}
+      <h4>
+        {birthday.name} {birthday.surname}
+      </h4>
+      <p>
+        <strong>Birthday on </strong>
+        {day}-{month}-{year}
+      </p>
+      <p className="birthday-note">{birthday.note}</p>
+      <img src={birthday.picture} alt="" width="100" height="100" />
     </div>
   );
 };
