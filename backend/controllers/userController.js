6139072c4ac1e72632dfce8daa5cb79401cc6f45
alimirhashimli1 <alimirhashimli@gmail.com>
2024-06-16 04:10:07 +0200

    created login token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,19 @@ const createToken = (_id) => {
 }
 
 const loginUser = async (req, res) => {
-    res.json({msg: "login user"})
+    const {email, password} = req.body
+    try {
+        const user = await User.login(email, password)
+
+        // create a token
+
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+
 }
 
 
@@ -17,7 +29,12 @@ const signupUser = async (req, res) => {
 
     try {
         const user = await User.signup(email, password)
-        res.status(200).json({email, user})
+
+        // create a token
+
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
     } catch (error) {
         res.status(400).json({error: error.message})
     }
